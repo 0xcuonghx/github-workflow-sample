da667a0c1d8b33618895d3805d307b96c6c7853b
cuonghx-gutech <cuonghx@gu-tech.com>
2023-10-20 16:31:20 +0700

    .

--- next-example/app/page.tsx
+++ next-example/app/page.tsx
@@ -2,7 +2,7 @@
 import { useEnvContext } from "next-runtime-env";
 
 export default function Home() {
-  const { NEXT_PUBLIC_CONTRACT_ADDRESS } = useEnvContext();
+  const { NEXT_PUBLIC_CHAIN_ID } = useEnvContext();
 
-  return <>{NEXT_PUBLIC_CONTRACT_ADDRESS}</>;
+  return <>{NEXT_PUBLIC_CHAIN_ID}</>;
 }

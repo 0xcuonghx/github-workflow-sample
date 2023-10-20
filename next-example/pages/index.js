02b7ea0f2f411196d7273003b36af50c623507a9
cuonghx-gutech <cuonghx@gu-tech.com>
2023-10-20 11:52:13 +0700

    .

--- next-example/pages/index.js
+++ next-example/pages/index.js
@@ -4,11 +4,10 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
+import { env } from "next-runtime-env";
 
 export default function Home() {
   return (
-    <>
-      NEXT_PUBLIC_CONTRACT_ADDRESS: {window.__ENV.NEXT_PUBLIC_CONTRACT_ADDRESS}
-    </>
+    <>NEXT_PUBLIC_CONTRACT_ADDRESS: {env("NEXT_PUBLIC_CONTRACT_ADDRESS")}</>
   );
 }
